16bc0648bc5f3397995c5746b09a7b93986490b3
Godwin Agbon <agbonwinn@outlook.com>
2022-03-06 04:20:10 +0100

    Add project files.

---
+++ ServiceManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServiceManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
