c2b2dd602105bf1456c8d1fa0b17d65a82a75aeb
Godwin Agbon <agbonwinn@outlook.com>
2022-04-26 19:29:16 +0100

    update hourly bg

--- ServiceManager.Web/Program.cs
+++ ServiceManager.Web/Program.cs
@@ -118,7 +118,7 @@ try
     //use this for gb
     app.UseHangfireDashboard();
     //Recurrent Job
-   RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), Cron.DayInterval(1));
+   RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), Cron.Hourly(6));
 
 
     //Fire and Forget
