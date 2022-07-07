b07d6fba411f02b8a25f2a668d0711367d864069
Godwin Agbon <agbonwinn@yahoo.com>
2022-07-07 09:22:14 +0100

    new update2

--- ServiceManager.Web/Program.cs
+++ ServiceManager.Web/Program.cs
@@ -124,9 +124,10 @@ try
     //use this for gb
     app.UseHangfireDashboard();
     //Recurrent Job
-    //RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), Cron.Daily());
+    //RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), Cron.Hourly(12));
     //RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), Cron.MinuteInterval(6));
-    RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), "24* * * * *");//24 hours
+     RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), "0 */12 * * *");//12 hours//Daily 0 * 1 * *
+    //RecurringJob.AddOrUpdate<IGenRepo<Service, int>>(x => x.MonitorServiceAlert(), "*/720 * * * *");//24 hours
     //4 minutes =*/4 * * * *"
 
 
